9eee5d5acc748eb441bc9d3efabab154e854d1f9
geril <gergert.ilya@mail.ru>
2025-08-10 11:25:08 +0300

    upd

--- .config/nvim/lua/my/lazy.lua
+++ .config/nvim/lua/my/lazy.lua
@@ -17,7 +17,7 @@ require("lazy").setup("plugins", {
 	},
 	dev = {
 		-- directory where you store your local plugin projects
-		path = "~/my/code/nvim-plugins",
+		path = "~/code/oss",
 		fallback = false, -- Fallback to git when local plugin doesn't exist
 	},
 

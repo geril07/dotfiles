f744954c60fe77acaeecbbcc76cb113446a2b6ec
geril <gergert.ilya@mail.ru>
2025-08-13 21:22:41 +0300

    upd

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -111,6 +111,12 @@ return {
 
 			require("treesitter-context").setup({
 				enable = false, -- Enable this plugin (Can be enabled/disabled later via commands)
+				-- Avoid the sticky context from growing a lot.
+				max_lines = 3,
+				-- Match the context lines to the source code.
+				multiline_threshold = 1,
+				-- Disable it when the window is too small.
+				min_window_height = 20,
 			})
 		end,
 	},

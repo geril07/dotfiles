274cd20aec86e86c58560b1c5656fb3492add96f
geril <gergert.ilya@mail.ru>
2025-10-21 19:50:08 +0300

    avante

---
+++ .config/nvim/lua/plugins/avante.lua
@@ -0,0 +1,14 @@
+return {
+	{
+		"yetone/avante.nvim",
+		event = "VeryLazy",
+		enabled = false,
+		opts = {
+			provider = "opencode",
+		},
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+		},
+	},
+}

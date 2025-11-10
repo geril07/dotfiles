d7c09b5c74210942de0fced77ad1bba612e513bd
geril <gergert.ilya@mail.ru>
2025-11-10 19:48:52 +0300

    upd

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -269,19 +269,19 @@ return {
 						yaml = {
 							-- Schemas https://www.schemastore.org
 							schemas = {
-								["http://json.schemastore.org/gitlab-ci.json"] = { ".gitlab-ci.yml" },
-								["https://json.schemastore.org/bamboo-spec.json"] = {
+								["http://www.schemastore.org/gitlab-ci.json"] = { ".gitlab-ci.yml" },
+								["https://www.schemastore.org/bamboo-spec.json"] = {
 									"bamboo-specs/*.{yml,yaml}",
 								},
 								["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = {
 									"docker-compose*.{yml,yaml}",
 								},
-								["http://json.schemastore.org/github-workflow.json"] = ".github/workflows/*.{yml,yaml}",
-								["http://json.schemastore.org/github-action.json"] = ".github/action.{yml,yaml}",
-								["http://json.schemastore.org/prettierrc.json"] = ".prettierrc.{yml,yaml}",
-								["http://json.schemastore.org/stylelintrc.json"] = ".stylelintrc.{yml,yaml}",
-								["http://json.schemastore.org/circleciconfig"] = ".circleci/**/*.{yml,yaml}",
-								["http://json.schemastore.org/lazydocker.json"] = "lazydocker/**/*.{yml,yaml}",
+								["http://www.schemastore.org/github-workflow.json"] = ".github/workflows/*.{yml,yaml}",
+								["http://www.schemastore.org/github-action.json"] = ".github/action.{yml,yaml}",
+								["http://www.schemastore.org/prettierrc.json"] = ".prettierrc.{yml,yaml}",
+								["http://www.schemastore.org/stylelintrc.json"] = ".stylelintrc.{yml,yaml}",
+								["http://www.schemastore.org/circleciconfig"] = ".circleci/**/*.{yml,yaml}",
+								["http://www.schemastore.org/lazydocker.json"] = "lazydocker/**/*.{yml,yaml}",
 								["https://taskfile.dev/schema.json"] = "**/Taskfile.{yml,yaml}",
 							},
 						},

ffea6deb5abec0c066ae7147571e94affecafb9e
geril <gergert.ilya@mail.ru>
2026-01-14 12:13:44 +0300

    ReloadBufs cmd

--- .config/nvim/lua/my/commands.lua
+++ .config/nvim/lua/my/commands.lua
@@ -7,3 +7,19 @@ end, {})
 create_command("SSelect", function()
 	require("persistence").select()
 end, {})
+
+function ReloadAllBuffers()
+	local current = vim.api.nvim_get_current_buf()
+	for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+		if vim.api.nvim_buf_is_loaded(buf) and vim.bo[buf].buftype == "" then
+			vim.api.nvim_buf_call(buf, function()
+				vim.cmd("silent! edit")
+			end)
+		end
+	end
+	vim.api.nvim_set_current_buf(current)
+end
+
+create_command("ReloadBufs", function()
+	ReloadAllBuffers()
+end, {})

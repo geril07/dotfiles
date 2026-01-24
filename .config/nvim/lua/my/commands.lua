88a9a65dcc4346db111a508669caa2c8ad67be22
geril <gergert.ilya@mail.ru>
2026-01-24 16:02:29 +0300

    lsplog nvim command

--- .config/nvim/lua/my/commands.lua
+++ .config/nvim/lua/my/commands.lua
@@ -23,3 +23,9 @@ end
 create_command("ReloadBufs", function()
 	ReloadAllBuffers()
 end, {})
+
+vim.api.nvim_create_user_command("LspLog", function()
+	vim.cmd(string.format("tabnew %s", vim.lsp.log.get_filename()))
+end, {
+	desc = "Opens the Nvim LSP client log.",
+})

322ecd31d5a1631192c55f4bd7ad09b7da76dfda
geril <gergert.ilya@mail.ru>
2026-03-26 15:59:19 +0300

    codediff.nvim options

--- .config/nvim/lua/plugins/diff.lua
+++ .config/nvim/lua/plugins/diff.lua
@@ -7,5 +7,21 @@ return {
 	{
 		"esmuellert/codediff.nvim",
 		cmd = "CodeDiff",
+		opts = {
+			diff = {
+				layout = "inline",
+			},
+
+			explorer = {
+				view_mode = "tree",
+				focus_on_select = true,
+			},
+
+			keymaps = {
+				explorer = {
+					select = "o",
+				},
+			},
+		},
 	},
 }
